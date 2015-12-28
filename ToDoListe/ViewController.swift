4d763e526833acefe5b120b3e9a3a41bae256b81
Michael Stroh <schleppi@michael-mbp.fritz.box>
2015-12-28 13:39:36 +0100

    ToDoListe als Prototyp entsprechend eines Tutorials

--- ToDoListe/ViewController.swift
+++ ToDoListe/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDos: [String] = ["Italienisch Vokabeln lernen", "Web-Programmierung lernen"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Zelle innerhalb der TableView bei der TableView selbst registrieren
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "toDoCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Vorab registrierte Klasse holen
+        let cell = tableView.dequeueReusableCellWithIdentifier("toDoCell", forIndexPath: indexPath) as UITableViewCell
+        // Array-Eintrag jeweils in Cell speichern
+        cell.textLabel?.text = toDos[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
